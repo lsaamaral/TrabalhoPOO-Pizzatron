a38f3d2fde554f6c2fc37c2526a40109f1df11c5
Isabella Amaral <isabellaamaral@outlook.pt>
2024-11-19 23:28:23 -0300

    Adicionando as imagens

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from database import BancoDeDados
 from usuario import Usuario
-
+import pygame
+"""
 banco = BancoDeDados("mongodb+srv://usertrabpoo:pizzatron3000@cluster0.ve3le.mongodb.net/?retryWrites=true&w=majority", "jogo")
 usuario = Usuario(banco, "jogadorteste1", "senhateste1")
 usuario.cadastrar()
@@ -10,3 +11,24 @@ usuario.fazer_login()
 usuario.registrar_coins(501)
 
 usuario.exibir_melhores_coins()
+"""
+pygame.init()
+
+screen = pygame.display.set_mode((1200, 750))
+pygame.display.set_caption("Pizzatron 3000")
+
+background = pygame.image.load("Sprites/Backgrounds/BgMenuInicial.png")
+background = pygame.transform.scale(background, (1200, 750))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.blit(background, (0, 0))
+
+    pygame.display.update()
+
+pygame.quit()
+
