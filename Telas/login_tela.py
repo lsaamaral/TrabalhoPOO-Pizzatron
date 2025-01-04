c582b84786da00c01fa176f4134a2dfae9cb4aa0
juliacamelo <74781632+juliacamelo@users.noreply.github.com>
2025-01-04 18:13:24 -0300

    Update login_tela.py

--- Telas/login_tela.py
+++ Telas/login_tela.py
@@ -6,9 +6,10 @@ class LoginTela(Tela):
     def __init__(self, tela, banco):
         self.tela = tela
         self.banco = banco
-        self.fonte = pygame.font.Font("Assets/BurbankSmallBold.ttf", 32)
+        self.fonte = pygame.font.Font("Assets/Roboto-Regular.ttf", 32)
         self.background = pygame.image.load("Assets/Backgrounds/MenuLogin.png")
         self.background = pygame.transform.scale(self.background, (1200, 750))
+        self.botao_inicio = pygame.Rect(300, 400, 200, 50) 
         self.input_ativo = {"login": False, "senha": False}
         self.inputs = {"login": "", "senha": ""}
         self.msg_erro = ""
@@ -25,6 +26,11 @@ class LoginTela(Tela):
         self.tela.blit(senha_box, (310, 310))
         self.tela.blit(erro_msg, (300, 400))
 
+        #função para desenhar o botão que leva ao menu inicial:
+        pygame.draw.rect(self.tela, (0, 255, 0), self.botao_inicio)
+        inicio_texto = self.fonte.render("Menu Inicial", True, (0, 0, 0))
+        self.tela.blit(inicio_texto, (self.botao_inicio.x + 20, self.botao_inicio.y + 10))
+
     def handle_input(self, evento):
         if evento.type == pygame.MOUSEBUTTONDOWN:
             if pygame.Rect(300, 200, 200, 40).collidepoint(evento.pos):
@@ -33,20 +39,28 @@ class LoginTela(Tela):
             elif pygame.Rect(300, 300, 200, 40).collidepoint(evento.pos):
                 self.input_ativo["login"] = False
                 self.input_ativo["senha"] = True
+            elif self.botao_inicio.collidepoint(evento.pos):
+                return "menu"
             else:
                 self.input_ativo = {"login": False, "senha": False}
 
         if evento.type == pygame.KEYDOWN:
-            if self.input_ativo["login"]:
-                if evento.key == pygame.K_BACKSPACE:
+            #lista de caracteres validos (apenas leras e numeros):
+            caracteres_validos = ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                                  "0123456789")
+            #permite apagar o que está sendo escrito:
+            if evento.key == pygame.K_BACKSPACE:
+                if self.input_ativo["login"]:
                     self.inputs["login"] = self.inputs["login"][:-1]
-                else:
-                    self.inputs["login"] += evento.unicode
-            elif self.input_ativo["senha"]:
-                if evento.key == pygame.K_BACKSPACE:
+                elif self.input_ativo["senha"]:
                     self.inputs["senha"] = self.inputs["senha"][:-1]
-                else:
+            #permite escrever somente os caracteres validos:
+            elif evento.unicode in caracteres_validos:
+                if self.input_ativo["login"]:
+                    self.inputs["login"] += evento.unicode
+                elif self.input_ativo["senha"]:
                     self.inputs["senha"] += evento.unicode
+
 
     def authenticate(self):
         usuario = Usuario(self.banco, self.inputs["login"], self.inputs["senha"])
@@ -58,6 +72,11 @@ class LoginTela(Tela):
             print("Usuario nao encontrado. Tentando cadastrar...")
             usuario.cadastrar()
             usuario.fazer_login()  # Tenta login depois do cadastro
+            if usuario.id:
+                return usuario
+            else:
+                self.msg_erro = "Erro ao cadastrar ou autenticar usuario."
+                return None
             if usuario.id:
                 return usuario
             else:
